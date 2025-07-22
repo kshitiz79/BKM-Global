003a216fc8ea2bd2e11a00d177ba794279bf3ac0
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-07-23 00:31:17 +0530

    d

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -27,7 +27,7 @@ const AdminLogin = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:8000/api/auth/login', {
+      const response = await fetch('https://bkm-backed.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
